8a2f74655aded9e0d84e35e4d9b8f06108694761
Tu0117 <142536481+Tu0117@users.noreply.github.com>
2023-08-20 14:48:17 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,76 @@
+/**
+ *  
+ */
+var settings = {
+    // 愛心配置
+    love: {
+        length: 600,  // 
+        duration: 2,  // 
+        velocity: 100,  //
+        effect: -0.75, // 
+        size: 32, // 
+        color: "#ffadbb" // 
+    },
+    // 開始按鈕配置
+    button: {
+        // 
+        text: "打開禮物",
+        // 
+        color: "#FFF",
+        // 
+        borderColor: "red",
+        // 
+        width: "120",
+        // 
+        height: "40",
+        // 
+        size: "18",
+        // 
+        borderRadius: "5"
+    },
+    // 
+    bg: {
+        color: "#000"
+    },
+    // 
+    music: {
+        // 
+        src: "123.mp3"
+    },
+    // 文字配置
+    text: {
+        // 
+        content: "七夕情人節快樂",
+        // 
+        size: "100",
+        // 
+        width: "100",
+        // 
+        color: "#FFF",
+        // 
+        bold: "1",
+        // 
+        left: "20",
+        // 
+        top: "20",
+        // 
+        opacity: ".6"
+    },
+    // 圖片配置
+    image: {
+        // 
+        src: "",
+        // 
+        width: "100",
+        // 
+        height: "100",
+        // 
+        top: "200",
+        // 
+        left: "200",
+        // 
+        borderRadius: "50%",
+        //
+        opacity: ".5"
+    }
+};
